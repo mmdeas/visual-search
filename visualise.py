546bea2b0d9e88f114cc563feae5a1aa0f5677a5
Miguel Martinez de Aguirre <miguel.aguirre@aguirredevelopment.co.uk>
2014-03-07 13:00:30 +0000

    Added ability to have multiple searches, changed the colouring to be relative instead of absolute, increased all costs by 1 to make Manhattan distance heuristic admissible and added more search types.

--- visualise.py
+++ visualise.py
@@ -75,37 +75,68 @@ class Astar(Search):
 	def h(self, node):
 		return abs(node[0] - target[0]) + abs(node[1] - target[1])
 
+class BestFirst(Search):
+	"""Besf-first search across image."""
+	def __init__(self, target):
+		super(BestFirst, self).__init__(target)
+		self.queue = Queue.PriorityQueue()
+		self.visited = set()
+	def put(self, node, cost):
+		self.queue.put((self.h(node), node, cost))
+	def h(self, node):
+		return abs(node[0] - target[0]) + abs(node[1] - target[1])
+
+class LowestCost(Search):
+	def __init__(self, target):
+		super(LowestCost, self).__init__(target)
+		self.queue = Queue.PriorityQueue()
+		self.visited = set()
+	def put(self, node, cost):
+		self.queue.put((cost, node, cost))
+
 def calculateCost(costs, n1, n2):
 	c = 0;
 	for i in xrange(3):
 		a = int(costs.get(n1[0], n1[1]).split(" ")[i])
 		b = int(costs.get(n2[0], n2[1]).split(" ")[i])
 		c += abs(a - b)
-	return c
+	return c + 1
 
 def next():
-	global search, costs
-	if search.empty():
-		return
-	cost, node = search.get()
-	photo.put('green', node)
-	if node == target:
-		return
-	for x in [-1, 0, 1]:
-		for y in [-1, 0, 1]:
-			child = (node[0]+x, node[1]+y)
-			if child[0] < 0 or child[1] < 0 or child[0] > target[0] or child[1] > target[1]:
-				continue
-			if search.isVisited(child):
-				continue
-			search.put(child, cost + calculateCost(costs, node, child))
-			photo.put('black', child)
+	global searches, costs, colours
+	for i in xrange(len(searches)):
+		search = searches[i]
+		colour = colours[i]
+		if search.empty():
+			return
+		cost, node = search.get()
+		colorify(node, colour)
+		if node == target:
+			return
+		for x in [-1, 0, 1]:
+			for y in [-1, 0, 1]:
+				child = (node[0]+x, node[1]+y)
+				if child[0] < 0 or child[1] < 0 or child[0] > costs.width()-1 or child[1] > costs.height()-1:
+					continue
+				if search.isVisited(child):
+					continue
+				search.put(child, cost + calculateCost(costs, node, child))
 	root.after(1, next)
+
+def colorify(node, colour):
+	global photo
+	current = [int(x) for x in photo.get(node[0], node[1]).split(" ")]
+	r,g,b = [(current[i]+colour[i])/2 for i in xrange(3)]
+	photo.put("#%02x%02x%02x" % (r,g,b), node)
 
 costs = photo.copy()
 target = (photo.width()-1, photo.height()-1)
 search = Astar(target)
 search.put((0, 0), 0)
+search2 = LowestCost(target);
+search2.put((0, 0), 0)
+searches = [search, search2];
+colours = [(0,255,0), (255,0,0)]
 next()
 
 root.mainloop();
