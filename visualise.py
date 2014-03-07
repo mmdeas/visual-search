8726b15b79ed8df8933b9c00fbda39e0ded33d4f
Miguel Martinez de Aguirre <miguel.aguirre@aguirredevelopment.co.uk>
2014-03-07 19:25:44 +0000

    Removed global variables and code. Removed dead code. Made test code only run when main. Added start function.

--- visualise.py
+++ visualise.py
@@ -1,33 +1,8 @@
 #! /usr/bin/python
 
 from Tkinter import *
-import sys
 import Queue
-
-if len(sys.argv) < 2:
-	sys.argv.append('black.gif')
-
-	# print "usage: {0} image.gif".format(sys.argv[0])
-	# exit(1)
-
-width = height = 0;
-
-if len(sys.argv) >= 3:
-	width = int(sys.argv[2])
-	height = int(sys.argv[2])
-
-if len(sys.argv) >= 4:
-	height = int(sys.argv[3])
-
-
-root = Tk()
-root.title("Search")
-
-photo = PhotoImage(file=sys.argv[1])
-
-canvas = Canvas(root, width=photo.width(), height=photo.height());
-canvas.create_image(0, 0, image=photo, anchor=NW)
-canvas.pack();
+from random import randint
 
 class Search(object):
 	"""Abstract Search object from which to inherit."""
@@ -102,15 +77,14 @@ def calculateCost(costs, n1, n2):
 		c += abs(a - b)
 	return c + 1
 
-def next():
-	global searches, costs, colours
+def next(searches, costs, colours, photo, root):
 	for i in xrange(len(searches)):
 		search = searches[i]
 		colour = colours[i]
 		if search.empty():
 			return
 		cost, node = search.get()
-		colorify(node, colour)
+		colorify(node, colour, photo)
 		if node == target:
 			return
 		for x in [-1, 0, 1]:
@@ -121,22 +95,41 @@ def next():
 				if search.isVisited(child):
 					continue
 				search.put(child, cost + calculateCost(costs, node, child))
-	root.after(1, next)
+	root.after(1, next, searches, costs, colours, photo, root)
 
-def colorify(node, colour):
-	global photo
+def colorify(node, colour, photo):
 	current = [int(x) for x in photo.get(node[0], node[1]).split(" ")]
 	r,g,b = [(current[i]+colour[i])/2 for i in xrange(3)]
 	photo.put("#%02x%02x%02x" % (r,g,b), node)
 
-costs = photo.copy()
-target = (photo.width()-1, photo.height()-1)
-search = Astar(target)
-search.put((0, 0), 0)
-search2 = LowestCost(target);
-search2.put((0, 0), 0)
-searches = [search, search2];
-colours = [(0,255,0), (255,0,0)]
-next()
+def start(searches, colours, photo, costs=None):
+	root = Tk()
+	root.title("Search: " + `photo`)
+	if type(photo) is str:
+		photo = PhotoImage(file=photo)
+	if costs is None:
+		costs = photo.copy()
+	canvas = Canvas(root, width=photo.width(), height=photo.height());
+	canvas.create_image(0, 0, image=photo, anchor=NW)
+	canvas.pack();
+	for search in searches:
+		if search.queue.empty:
+			search.put((randint(0, photo.width()-1), randint(0, photo.height())), 0)
+	next(searches, costs, colours, photo, root)
 
-root.mainloop();
+	root.mainloop()
+
+if __name__ == '__main__':
+	import sys
+	if len(sys.argv) < 2:
+		print "usage: {0} image.gif".format(sys.argv[0])
+		exit(1)
+
+	target = (0, 0)
+	search = Astar(target)
+	search2 = LowestCost(target);
+	search3 = DFS(target);
+	searches = [search, search2, search3];
+	colours = [(0,255,0), (255,0,0), (0,0,255)]
+
+	start(searches, colours, sys.argv[1])
