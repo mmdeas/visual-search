2e2a877579faae0acb3831d192d0e30db002e700
Miguel Martinez de Aguirre <miguel.aguirre@aguirredevelopment.co.uk>
2014-03-09 17:58:31 +0000

    Added Random, Bidirectional and A* search with a different heuristic. Changed the way colouring works to make activity more obvious. Removed dependency for module-level 'target' attribute in A* heuristic.

--- visualise.py
+++ visualise.py
@@ -58,7 +58,12 @@ class Astar(Search):
 	def put(self, node, cost):
 		self.queue.put((cost + self.h(node), node, cost))
 	def h(self, node):
-		return abs(node[0] - target[0]) + abs(node[1] - target[1])
+		return abs(node[0] - self.target[0]) + abs(node[1] - self.target[1])
+
+class Astar2(Astar):
+	"""Astar with different heuristic."""
+	def h(self, node):
+		return abs(node[0] - self.target[0]) + abs(node[1] - self.target[1]) + calculateCost(self.costs, node, self.target)
 
 class BestFirst(Search):
 	"""Besf-first search across image."""
@@ -104,7 +109,55 @@ class TwoWayUniformCost(Search):
 	def empty(self):
 		return self.active[0].empty()
 
+class Bidirectional(Search):
+	"""Alternative to TwoWayUniformCost."""
+	def __init__(self, target):
+		super(Bidirectional, self).__init__(target)
+		self.queue = Queue.PriorityQueue()
+		self.visited = (set(), set()) # (initial, target)
+		self.active = 0
+		self.first = True
+	def __repr__(self):
+		return ''.join(["<",self.name," ",`len(self.visited[0]) + len(self.visited[1])`,">"])
+	def put(self, node, cost):
+		self.queue.put((cost, node, cost, self.active))
+		if self.first:
+			self.queue.put((0, self.target, 0, 1))
+			self.first = False
+	def get(self):
+		node = self.queue.get()
+		while node[1] in self.visited[node[3]]:
+			node = self.queue.get()
+		self.visited[node[3]].add(node[1])
+		self.active = node[3]
+		return node[2], node[1]
+	def isVisited(self, node):
+		return node in self.visited[self.active]
+	def isTarget(self, node):
+		return node in self.visited[0 if self.active == 1 else 1]
 
+class Random(Search):
+	"""Expands nodes randomly."""
+	def __init__(self, target):
+		super(Random, self).__init__(target)
+		self.queue = list()
+		self.visited = set()
+	def put(self, node, cost):
+		self.queue.append((cost, node))
+	def get(self):
+		i = randint(0, len(self.queue)-1)
+		self.queue[i], self.queue[-1] = self.queue[-1], self.queue[i]
+		node = self.queue.pop()
+		while node[1] in self.visited:
+			i = randint(0, len(self.queue)-1)
+			self.queue[i], self.queue[-1] = self.queue[-1], self.queue[i]
+			node = self.queue.pop()
+		self.visited.add(node[1])
+		return node
+	def empty(self):
+		return len(self.queue) == 0
+
+		
 
 def calculateCost(costs, n1, n2):
 	c = 0;
@@ -149,7 +202,8 @@ def next(searches, costs, colours, photo, root, output):
 def colorify(node, colour, photo):
 	current = [int(x) for x in photo.get(node[0], node[1]).split(" ")]
 	r,g,b = [(current[i]+colour[i])/2 for i in xrange(3)]
-	photo.put("#%02x%02x%02x" % (r,g,b), node)
+	photo.put("#%02x%02x%02x" % colour, node)
+	photo.root.after(500, lambda c:photo.put("#%02x%02x%02x" % c, node), (r,g,b))
 
 def start(searches, colours, photo, costs=None, output=None):
 	root = Tk()
@@ -158,10 +212,12 @@ def start(searches, colours, photo, costs=None, output=None):
 		photo = PhotoImage(file=photo)
 	if costs is None:
 		costs = photo.copy()
+	photo.root = root
 	canvas = Canvas(root, width=photo.width(), height=photo.height());
 	canvas.create_image(0, 0, image=photo, anchor=NW)
 	canvas.pack();
 	for search in searches:
+		search.costs = costs
 		if search.empty():
 			# search.put((randint(0, photo.width()-1), randint(0, photo.height())), 0)
 			search.put((photo.width()-1, photo.height()-1), 0)
@@ -180,7 +236,7 @@ if __name__ == '__main__':
 		output = None
 
 	target = (0, 0)
-	searches = [UniformCost(target), Astar(target), TwoWayUniformCost(target)]
-	colours = [(0,255,0), (255, 0, 0), (0, 0, 255), (0, 255, 255)]
+	searches = [Astar(target), Astar2(target), Bidirectional(target)]
+	colours = [(0,255,0), (255, 0, 0), (0, 0, 255), (0, 255, 255), (255, 255, 0)]
 
 	start(searches, colours, sys.argv[1], output=output)
